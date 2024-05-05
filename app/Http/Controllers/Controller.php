45fac715421e2d2c0a494e57af32b69090118b20
Vin Valentino <42009589+VinValentinoo@users.noreply.github.com>
2024-05-05 18:45:10 +0200

    Dashboard updates

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +10,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function dashboard() {
+        $blogs = Blog::where('user_id', '=', auth()->user()->id)->get();
+        return view('dashboard', ['blogs' => $blogs]);
+    }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,4 +14,24 @@
             </div>
         </div>
     </div>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="grid grid-cols-3 gap-5 p-3">
+                       
+                        @foreach ($blogs as $blog)
+                            <a href="/{{$blog->id}}" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{$blog->title}}</h5>
+                                <p class="font-normal text-gray-700 dark:text-gray-400">{{$blog->short_description}}</p>
+                                <p class="font-normal text-gray-700 dark:text-gray-400">{{$blog->user->name}} - {{$blog->created_at}}</p>
+                            </a>
+                        @endforeach
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
